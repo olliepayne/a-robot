204178574e4d7a2ecb6b163296c7deb54c9708dc
olliepayne <olliepayne@me.com>
2021-03-20 18:58:45 -0700

    handle state

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const roads = [
 function buildGraph(edges) {
   let graph = Object.create(null)
   function addEdge(from, to) {
-    if(graph[from] == null) {
+    if (graph[from] == null) {
       grpah[from] = graph[to]
     } else {
       graph[from].push(to)
     }
   }
-  for(let [from, to] of edges.map(r => r.split('-'))) {
+  for (let [from, to] of edges.map(r => r.split('-'))) {
     addEdge(from, to)
     addEdge(to, from)
   }
@@ -25,3 +25,23 @@ function buildGraph(edges) {
 }
 
 const roadGraph = buildGraph(roads)
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place
+    this.parcels = parcels
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p
+        return { place: destination, address: p.address }
+      })
+      .filter(p => p.place != p.address)
+      return new VillageState(destination, parcels)
+    }
+  }
+}
