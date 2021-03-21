cc8126033e86eb40bae63e91b6594700ffd7f31c
olliepayne <olliepayne@me.com>
2021-03-20 18:53:43 -0700

    build graph

--- app.js
+++ app.js
@@ -9,5 +9,19 @@ const roads = [
 ]
 
 function buildGraph(edges) {
-
+  let graph = Object.create(null)
+  function addEdge(from, to) {
+    if(graph[from] == null) {
+      grpah[from] = graph[to]
+    } else {
+      graph[from].push(to)
+    }
+  }
+  for(let [from, to] of edges.map(r => r.split('-'))) {
+    addEdge(from, to)
+    addEdge(to, from)
+  }
+  return graph
 }
+
+const roadGraph = buildGraph(roads)
